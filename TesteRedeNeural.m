95cc599ebbf888e7232465b076c0e2a425e86192
Gabriel Oliveira Pimentel <gabrieloliveirapimentel@hotmail.com>
2024-06-02 19:11:16 +0100

    net

--- TesteRedeNeural.m
+++ TesteRedeNeural.m
@@ -15,26 +15,23 @@ layers = [
 
  imageInputLayer([300 300 3])
 
- convolution2dLayer(30,8,'Padding','same')
+ convolution2dLayer(3,8,'Padding','same')
  batchNormalizationLayer
  reluLayer
  maxPooling2dLayer(2,'Stride',2)
 
- convolution2dLayer(30,16,'Padding','same')
+ convolution2dLayer(3,16,'Padding','same')
  batchNormalizationLayer
  reluLayer
  maxPooling2dLayer(2,'Stride',2)
 
- convolution2dLayer(30,32,'Padding','same')
+ convolution2dLayer(3,32,'Padding','same')
  batchNormalizationLayer
  reluLayer
 
  fullyConnectedLayer(3)
  softmaxLayer
  classificationLayer];
-
-lgraph = layerGraph(layers);
-plot(lgraph)
 
 options = trainingOptions('adam', ...
  'MaxEpochs',20, ...
